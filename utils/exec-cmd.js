ba3ca93ab90fbaa1dc576c4ee00bc06c986d77f6
Yves Daaboul <yves.daaboul@protonmail.com>
2024-12-06 20:58:44 +0200

    [v0.1] Removed unused variable

--- utils/exec-cmd.js
+++ utils/exec-cmd.js
@@ -2,7 +2,7 @@ const cp = require("child_process");
 
 function executeCommand(command, options) {
   return new Promise((resolve, reject) => {
-    cp.exec(command, options, (error, stdout, stderr) => {
+    cp.exec(command, options, (error, stdout) => {
       if (error) {
         reject(error);
         return;
